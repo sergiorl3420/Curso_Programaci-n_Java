dfe29ffd1f28c9d8d9134e00ee27552c740c0279
sergiorl3420 <118071839+sergiorl3420@users.noreply.github.com>
2022-11-12 19:43:48 +0100

    Add files via upload

---
+++ Ejemplo01_01_SergioRamirez.java
@@ -0,0 +1,8 @@
+public class Ejemplo01_01_SergioRamirez {
+    public static void main(String[] args) {
+    //se corresponde con un método y no una clase, para que sea una clase tiene que estar definido
+    //como class
+    System.out.println("Bienvenido al curso de programación Java");
+        }    
+}
+
---
+++ Ejemplo01_02_SergioRamirez.java
@@ -0,0 +1,9 @@
+public class Ejemplo01_02_SergioRamirez {
+    public static void main(String[] args) {
+    //se corresponde con un método y no una clase, para que sea una clase tiene que estar definido
+    //como class
+    System.out.println("La vida es tan buena maestra,");
+    System.out.print("que si no aprendes la lección te la repites");
+        }    
+}
+
