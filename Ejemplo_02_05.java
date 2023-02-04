de31a9037cc24ad5105e298f98d525371ec79306
sergiorl3420 <118071839+sergiorl3420@users.noreply.github.com>
2023-02-04 19:14:37 +0100

    Add files via upload

---
+++ Ejemplo_02_05.java
@@ -0,0 +1,12 @@
+public class Ejemplo_02_05 {
+    public static void main(String[] args) {
+        //Pequeño ejercicio para comprobar como funciona el operador ++ y -- cuando se coloca antes o después de la
+        //variable, viendo como la saca y como modifica en este caso el valor de la variable n. Un ejercicio sencillo.
+        int n=8;
+        System.out.print(n);
+        System.out.print("/");
+        System.out.print(n++);
+        System.out.print("/");
+        System.out.print(n);
+    }
+}
