8c22f1cd346b4b2d8d1eefa9820e8bc943c22714
sergiorl3420 <118071839+sergiorl3420@users.noreply.github.com>
2023-02-04 21:59:25 +0100

    Delete Ejemplo01_03_SergioRamirez.java

--- Ejemplo01_03_SergioRamirez.java
+++
@@ -1,8 +0,0 @@
-public class Ejemplo01_03_SergioRamirez {
-    public static void main(String[] args) {
-    //se corresponde con un método y no una clase, para que sea una clase tiene que estar definido
-    //como class
-    System.out.println("Estoy aprendiendo a programar bien en Java");
-        }    
-}
-
