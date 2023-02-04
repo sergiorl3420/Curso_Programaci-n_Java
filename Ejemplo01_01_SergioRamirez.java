f79d411db768be75d0ee0967a187c03feb188216
sergiorl3420 <118071839+sergiorl3420@users.noreply.github.com>
2023-02-04 21:59:14 +0100

    Delete Ejemplo01_01_SergioRamirez.java

--- Ejemplo01_01_SergioRamirez.java
+++
@@ -1,8 +0,0 @@
-public class Ejemplo01_01_SergioRamirez {
-    public static void main(String[] args) {
-    //se corresponde con un método y no una clase, para que sea una clase tiene que estar definido
-    //como class
-    System.out.println("Bienvenido al curso de programación Java");
-        }    
-}
-
