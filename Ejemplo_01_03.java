871e26da699001641db5b94a9b0a43b3f40ade49
sergiorl3420 <118071839+sergiorl3420@users.noreply.github.com>
2023-02-04 23:55:09 +0100

    Add files via upload

---
+++ Ejemplo_01_03.java
@@ -0,0 +1,8 @@
+public class Ejemplo_01_03 {
+    public static void main(String[] args) {
+    //se corresponde con un método y no una clase, para que sea una clase tiene que estar definido
+    //como class
+    System.out.println("Estoy aprendiendo a programar bien en Java");
+        }    
+}
+
