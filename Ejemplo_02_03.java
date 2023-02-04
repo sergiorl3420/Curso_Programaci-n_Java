826ac21498651bee41507513f7367b8757424507
sergiorl3420 <118071839+sergiorl3420@users.noreply.github.com>
2023-02-04 18:17:33 +0100

    Add files via upload

---
+++ Ejemplo_02_03.java
@@ -0,0 +1,9 @@
+public class Ejemplo_02_03 {
+    public static void main(String[] args) {
+        int num=1;
+        boolean largo;
+        String primerApellido="Ramírez";
+        largo=primerApellido.length() > num;
+        System.out.print(largo);
+    }
+}
