81c35b561e375c2f4cf4c0a4006ccb8772ace4a3
sergiorl3420 <118071839+sergiorl3420@users.noreply.github.com>
2023-02-19 12:25:50 +0100

    Delete Ejemplo_04_01_Modificado.java

--- Ejemplo_04_01_Modificado.java
+++
@@ -1,9 +0,0 @@
-package Ejemplo_04_01_Modificado;
-public class Ejemplo_04_01_Modificado {
-	public static void main (String args[]) {
-		for (int i=0;i<args.length;i++) {
-			String s=args[i];
-			System.out.println(s);
-		}
-	}	
-}
